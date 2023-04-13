ba267cdcce34dd662039fae5ebbe0f307fd34950
Ale-Campos <alejandrocampos9092@gmail.com>
2023-04-12 22:26:03 -0300

    Arreglo de Git - Cambios

---
+++ index.js
@@ -0,0 +1,8 @@
+const navToggle = document.querySelector(".toggle")
+const navMenu = document.querySelector(".nav-menu")
+
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("nav-menu_actived")
+})
+
