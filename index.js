03a415cf72e20c4f7341d087c19ca41848da9c0d
Lautaro Cruz Srur <lautaro.cruzsrur@gmail.com>
2023-04-16 23:17:41 -0300

    js

--- index.js
+++ index.js
@@ -5,49 +5,48 @@ navToggle.addEventListener("click", () => {
   navMenu.classList.toggle("nav-menu_actived");
 });
 
-
 const nombre = document.getElementById("nombre");
 const email = document.getElementById("email");
 const asunto = document.getElementById("asunto");
 const descripcion = document.getElementById("descripcion");
 const form = document.getElementById("form");
-const parrafo = document.getElementById("warnings")
+const parrafo = document.getElementById("warnings");
 
-
-
-form.addEventListener("submit", e=>{
-  e.preventDefault()
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
   let warnings = "";
   let entrar = false;
   let regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-  parrafo.innerHTML= "";
-  if(nombre.Valuelength<6){
+  parrafo.innerHTML = "";
+  if (nombre.Valuelength < 6) {
     warnings += `El nombre no es valido <br>`;
-    entrar = true
+    entrar = true;
   }
-  if(!regexEmail.test(email.value)){
+  if (!regexEmail.test(email.value)) {
     warnings += `El Email no es valido <br>`;
-    entrar = true
-   }
-   if(asunto.value.length < 5){
+    entrar = true;
+  }
+  if (asunto.value.length < 5) {
     warnings += `El asunto es muy corto <br>`;
-    entrar = true
-   }
-   if(descripcion.value.length < 20){
+    entrar = true;
+  }
+  if (descripcion.value.length < 20) {
     warnings += `La descripcion es muy corta <br>`;
-    entrar = true
-   }
-   if(entrar = true){
+    entrar = true;
+  }
+  if ((entrar = true)) {
     parrafo.innerHTML = warnings;
-   }
-})
+  }
+});
 function mostrarImagen() {
   document.getElementById("ventanaEmergente").style.display = "block";
 }
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == document.getElementById("ventanaEmergente")) {
     document.getElementById("ventanaEmergente").style.display = "none";
   }
-}
-
-
+};
+window.addEventListener("scroll", function () {
+  var header = document.querySelector(".box-nav");
+  header.classList.toggle("abajo", window.scrollY > 0);
+});
